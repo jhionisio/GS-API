14ec7db6c582b08daccb8eb86aa1823db34bdb1e
jhionisio <mr.joaomarcelo@gmail.com>
2023-05-30 18:47:13 -0300

    START

---
+++ src/test/java/com/global_solution/GSAPI/GsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global_solution.GSAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
